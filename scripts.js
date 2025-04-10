d9bf4695f44a94a254f3b2d70a0a11f0c8db06ea
Jefferson Formigoni Araujo <jeffersonformigoni86@gmail.com>
2025-04-10 12:30:35 -0300

    Finalizado projeto

--- scripts.js
+++ scripts.js
@@ -1,10 +1,26 @@
 let body = document.querySelector("body")
 let tenis = document.querySelector(".imagem-tenis")
+let texto = document.querySelector("h1")
+let preco = document.querySelector("h4")
+
 // document = html
 // querySelector = pegador / selecionador
 
 function mudarVisual(cor, imagem){
+    tenis.classList.add("troca-efeito")
     
     body.style.background = cor
-    tenis.src = imagem
+    texto.style.color = cor // Adiciona a troca de cor do h1
+    preco.style.color = cor // Adiciona a troca de cor do h4
+
+
+    
+
+    // contador de tempo
+    setTimeout(() => {
+        tenis.src = imagem
+        tenis.classList.remove("troca-efeito")
+    }, 500);
+
+    // 1000 milisegundos = 1segundo
 }
